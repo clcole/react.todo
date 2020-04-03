47f323f0f6b259da105d49ad6bbd6113deea0e43
CLCole <clcole@gmail.com>
2020-04-02 20:04:20 -0500

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-## The Hello World of React!
-
-A Todo List written in React.
-The browser local storage is used to persist the app data.
---
+++ precache-manifest.5a074c786c8ed9272288d4aff44c50b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68cd876e776f5fbd167f455157c264e",
+    "url": "/react.todo/index.html"
+  },
+  {
+    "revision": "f75aef282e7afd53b460",
+    "url": "/react.todo/static/css/main.c4d70456.chunk.css"
+  },
+  {
+    "revision": "e56ccfba225502a67458",
+    "url": "/react.todo/static/js/2.9c18e05c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react.todo/static/js/2.9c18e05c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f75aef282e7afd53b460",
+    "url": "/react.todo/static/js/main.5159104e.chunk.js"
+  },
+  {
+    "revision": "bbd3709a31e824d6505b",
+    "url": "/react.todo/static/js/runtime-main.83a79524.js"
+  }
+]);
